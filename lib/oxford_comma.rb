65a7716e4e20e4dc0608ff1cf3af777deac93d37
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-14 04:52:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
   when 1 
     return array.join
   when 2 
-    return  array[-2]<<("and")
+    return  
+    array[-2]<<("and")
     array.join
   else 
     return array[0..-2].join(', ') + ", and " + array[-1]
